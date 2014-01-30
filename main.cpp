b3fdc20aaf1b8bec588bc21464b7f4467707933f
Dustin Dodson <dustinleedodson@gmail.com>
2014-01-29 19:26:55 -0900

    Added main.cpp from SVN part of assignment

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+cout << "Hi world. This is the CS372 example subversion project for HW1." << endl;
+cout << "The following people get credit for this part of the assignment:" << endl;
+cout << "Chris \"the cookie man\" Hartman" << endl;
+cout << "Dustin \"INSERT NICKNAME HERE\" Dodson" << endl;
+cout << "Tim \"^[.\\s]*$\" Slauson" << endl;
+
+return 0;
+}
